814feca1c7ea2fd1382ebd7d4faa8a89321ed37f
Hrachya Movsisyan <hm@bic.com.de>
2024-06-12 16:15:36 +0200

    edit action of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-    
+    before_action :set_user, except: [:new, :create]
+
     def new
         @user = User.new
     end
@@ -14,7 +15,24 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+    end
+
+    def update
+        if @user.update(user_params)
+            flash[:notice] = "Account Information of #{@user.username} was successfully updated"
+            redirect_to articles_path
+        else
+            render "edit"
+        end
+
+    end
+
     private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
     
     def user_params
         params.require(:user).permit(:username, :email, :password)
