2702cfcd5f29deeb8a71a8a4ddff70f43c6ea19f
Hrachya Movsisyan <hm@bic.com.de>
2024-06-12 16:05:52 +0200

    add users create action and frontend changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,20 @@
 class User < ApplicationRecord
-    has_many :articles
+    has_many :articles, dependent: :destroy
+    before_save :email_to_downcase
     validates :username, presence: true, 
         uniqueness: { case_sensitive: false }, 
         length: { minimum: 3, maximum: 25 }
         VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, 
         uniqueness: { case_sensitive: false }, 
-        length: { maximum: 105 },
+        length: { maximum: 205 },
         format: { with: VALID_EMAIL_REGEX }
+        
+    private
+    
+    def email_to_downcase
+        self.email = email.downcase
+    end
 
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root "pages#home"
   get "about", to: "pages#about"
   resources :articles
+
+  # Users
+  resources :users, except: [ :new ]
+  get "signup", to: "users#new"
 end
